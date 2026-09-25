86a2e14ee799561e355ec7366c3a6e01c83f33bc
Riyachechi <156057648+Riyachechi@users.noreply.github.com>
2026-09-25 23:07:09 +0530

    Fix import statement for database module

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
 from sqlalchemy.sql import func
 
-from .database import Base
+from database import Base
 
 
 class Order(Base):
